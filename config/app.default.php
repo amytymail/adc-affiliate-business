de193dccf65921fc70e9ee999c1817a32aec111d
Amit Shokeen <amitshokeen.dev@gmail.com>
2017-02-21 23:03:41 +0530

    Template added.

--- config/app.default.php
+++ config/app.default.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+    'debug' => false,
 
     /**
      * Configure basic information about the application.
